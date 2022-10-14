1731412931b2c6e7aeae41ffc674063929b5caeb
Mateo Corso <mcorso@student.42.fr>
2022-10-14 15:51:30 +0200

    Same as prev commit

--- src/lib/visualiser/VisualiserStore.ts
+++ src/lib/visualiser/VisualiserStore.ts
@@ -1,10 +1,12 @@
+import FileStore from "$lib/file_management/FileStoreModule";
 import type { NodeFile } from "$lib/file_management/file_classes";
-
-let currentSelectedFile: NodeFile;
-const subscribers: Function[] = [];
+import type { Tweened } from "svelte/motion";
+import { tweened } from "svelte/motion";
+import { quartOut } from "svelte/easing";
 
 interface Toggler {
 	toggle: boolean,
+	widthCursor: Tweened<number>
 	subscribers: Function[],
 	subscribe: (callback: (toggle: boolean) => void) => Function,
 	set: () => void,
@@ -12,12 +14,16 @@ interface Toggler {
 
 export const toggleVisualiser: Toggler = {
 	toggle: false,
+	widthCursor: tweened(100, {
+		duration: 1000,
+		easing: quartOut
+	}),
 	subscribers: [],
 	subscribe: function (callback: (toggle: boolean) => void): Function{
-		subscribers.push(callback);
+		this.subscribers.push(callback);
 
 		return () => {
-			const index = subscribers.findIndex(
+			const index = this.subscribers.findIndex(
 				(subscribed: Function) => subscribed === callback
 			);
 			subscribers.splice(index, 1);
@@ -26,16 +32,21 @@ export const toggleVisualiser: Toggler = {
 
 	set: function () {
 		this.toggle = !this.toggle;
+		const newValueOfVisuWidth: number = 50 + (this.toggle ? 0 : 50);
+		this.widthCursor.set(newValueOfVisuWidth);
 		subscribers.forEach((toCall) => {
-			console.log("yes");
 			toCall(this.toggle);
 		})
 	}
 }
 
+let currentSelectedFile: NodeFile;
+const subscribers: Function[] = [];
+
 export default {
 	subscribe(callback: (file: NodeFile) => void): Function {
 		subscribers.push(callback);
+		callback(currentSelectedFile);
 
 		return () => {
 			const index = subscribers.findIndex((subscribed: Function) => subscribed === callback);
@@ -43,13 +54,16 @@ export default {
 		}
 	},
 
-	set(newSelectedFile: NodeFile) {
-		currentSelectedFile = newSelectedFile;
+	set(newSelectedFileName: string) {
+		if (currentSelectedFile === undefined ||
+			(currentSelectedFile !== undefined && newSelectedFileName !== currentSelectedFile.name)
+		) { currentSelectedFile = FileStore.getFileByName(newSelectedFileName); }
 		subscribers.forEach((toCall) => {
 			toCall(currentSelectedFile)
 		})
 	},
 
-
-	
+	get(): NodeFile {
+		return (currentSelectedFile);
+	},
 }
