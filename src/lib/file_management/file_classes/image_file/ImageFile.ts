695010fe381bf187ec9ac1c5debca72e78d1dd25
ArenorRaxx <mateocorso@gmail.com>
2022-10-19 13:06:33 +0200

    Refactoring image file sizing methods

--- src/lib/file_management/file_classes/image_file/ImageFile.ts
+++ src/lib/file_management/file_classes/image_file/ImageFile.ts
@@ -1,13 +1,10 @@
 import { NodeFile } from "..";
+import go, { Size } from "gojs"
 import type { ImageNodeData } from "../types";
 import ImageFileVisu from "./ImageFileVisu.svelte";
 
-type Size = {
-	width: number,
-	height: number
-}
 
-const BOUNDARY: Size = { width: 256, height: 144 }
+const BOUNDARY: Size = new go.Size(256, 144);
 
 export default class ImageFile extends NodeFile {
 	public constructor(file: File) {
@@ -48,7 +45,7 @@ export default class ImageFile extends NodeFile {
 			const img = new Image();
 			
 			img.onload = () => {
-				const imageSize: Size = { width: img.width, height: img.height };
+				const imageSize: Size = new Size(img.width, img.height);
 				const resized = this.resizeImageProportionally(imageSize, BOUNDARY);
 				resolve(resized);
 			}
@@ -60,9 +57,6 @@ export default class ImageFile extends NodeFile {
 		const widthRatio = boundary.width / realSize.width;
 		const heightRatio = boundary.height / realSize.height;
 		const ratioToApply = Math.min(widthRatio, heightRatio);
-		return {
-			width: realSize.width * ratioToApply,
-			height: realSize.height * ratioToApply
-		};
+		return new Size(realSize.width * ratioToApply, realSize.height * ratioToApply);
 	}
 }
